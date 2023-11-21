f5f219a443ba90297eaefe3277dbd497d034bc90
yoni <kkt1692@konkuk.ac.kr>
2023-11-21 15:54:04 +0900

    [WIP] : section2-problem12

---
+++ src/main/java/org/example/codingTest1/section2/problem12/Main.java
@@ -0,0 +1,24 @@
+package org.example.codingTest1.section2.problem12;
+
+import java.util.*;
+
+public class Main {
+
+    public int solution(int n, int m, int[][] array) {
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] array = new int[m][n];
+
+        Main solution = new Main();
+        int result = solution.solution(n, m, array);
+
+        System.out.println(result);
+    }
+}
